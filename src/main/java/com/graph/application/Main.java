deca231d4367b4f195fceb66f403fdc00a55a5e6
Paidamoyo Chapfuwa <ashgirlchapfuwa@gmail.com>
2015-10-13 12:32:53 +0200

    combine file processor and display results

--- src/main/java/com/graph/application/Main.java
+++ src/main/java/com/graph/application/Main.java
@@ -2,20 +2,26 @@ package com.graph.application;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 import com.graph.application.service.FileProcessor;
+import com.graph.application.view.CompanyPeopleGraph;
+import com.graph.domain.Company;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("app is running!");
+        System.out.println("app is running!" + "\n");
 
         Path pathCompany = Paths.get(args[0]);
         Path pathPeople = Paths.get(args[1]);
 
-        String people = new FileProcessor(pathCompany, pathPeople).companyPeopleGraph().toString();
+        List<Company> companies = new FileProcessor(pathCompany, pathPeople).companyPeopleGraph();
 
-        System.out.println("all people are:" + people);
+        StringBuilder companyPeopleGraph = new CompanyPeopleGraph(companies).display();
+
+        System.out.println("company people graph:" + "\n") ;
+        System.out.println(companyPeopleGraph);
 
     }
 
