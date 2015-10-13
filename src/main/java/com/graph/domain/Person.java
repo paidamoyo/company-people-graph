f7a62584b3292f6951ed4acf10ddc4ba79871395
Paidamoyo Chapfuwa <ashgirlchapfuwa@gmail.com>
2015-10-13 10:08:40 +0200

    implement comparable in person class

--- src/main/java/com/graph/domain/Person.java
+++ src/main/java/com/graph/domain/Person.java
@@ -7,7 +7,7 @@ import lombok.Value;
 
 @Value
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
-public class Person {
+public class Person implements Comparable {
 
     private final String name;
     private final String surname;
@@ -17,4 +17,35 @@ public class Person {
     public static Person from(String name, String surname, String email, Company company) {
         return new Person(name, surname, email, company);
     }
+
+    public String getCompanyCity() {
+        return this.getCompany().getCity();
+    }
+
+    public String getCompanyName() {
+        return this.getCompany().getName();
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        if(o == null){
+            return 1;
+        }
+        Person other = (Person) o;
+        if (this == other) {
+            return 0;
+        }
+        int name = this.name.compareTo(other.name);
+        int surname = this.surname.compareTo(other.surname);
+        int email = this.email.compareTo(other.email);
+
+        if (name != 0) {
+            return name;
+        } else if (surname != 0) {
+            return surname;
+        }
+        return email;
+    }
 }
+
+
